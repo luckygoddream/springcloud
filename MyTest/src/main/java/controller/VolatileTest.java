a19f72185b0d8da78692aeb446e40f2d07bbeca1
jiaowenqi <jwq0204@163.com>
2021-01-05 15:46:59 +0800

    学习新

--- MyTest/src/main/java/controller/VolatileTest.java
+++ MyTest/src/main/java/controller/VolatileTest.java
@@ -9,7 +9,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class VolatileTest {
 
-
+    //数据之间没数据依赖关系时，可能会指令重排序
     //volatile 保证jmm中线程之间变量的可见性，不保证原子性，保证有序性（禁止指令重排序）
 
     public static void main(String[] args) {
@@ -17,13 +17,13 @@ public class VolatileTest {
 
         AtomicInteger integer = new AtomicInteger();
         integer.addAndGet(10);
-//        MyData myData = new MyData();
+        MyData myData = new MyData();
         MyData myData1 = new MyData();
 
         //volatile不保证原子性
-        for (int i = 1; i < 20; i++) {
+        for (int i = 1; i <= 20; i++) {
             new Thread(() -> {
-                for (int j = 1; j <1000 ; j++) {
+                for (int j = 1; j <= 1000 ; j++) {
                     myData1.addPlus();
                     myData1.atomicInt();
                 }
@@ -42,27 +42,27 @@ public class VolatileTest {
 
 
         //volatile保证可见性
-//        new Thread(() -> {
-//            System.out.println(Thread.currentThread().getName());
-//            try {
-//                //休眠3秒让主线程获取到值
-//                TimeUnit.SECONDS.sleep(3);
-//
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//            //将值改为60
-//            myData.add();
-//            System.out.println(myData.num);
-//        }, "first").start();
-//
-//        //未加volatile 线程间变量未同步会一直等待
-//        while (myData.num == 0) {
-//            //  System.out.println(System.currentTimeMillis());
-//        }
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName());
+            try {
+                //休眠3秒让主线程获取到值
+                TimeUnit.SECONDS.sleep(3);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            //将值改为60
+            myData.add();
+            System.out.println(myData.num);
+        }, "first").start();
+
+        //未加volatile 线程间变量未同步会一直等待
+        while (myData.num == 0) {
+            //  System.out.println(System.currentTimeMillis());
+        }
         //加上volatile线程间会同步变量值执行下面语句
-//        System.out.println(Thread.currentThread().getName());
-//        System.out.println(myData.num);
+        System.out.println(Thread.currentThread().getName());
+        System.out.println(myData.num);
 
     }
 }
