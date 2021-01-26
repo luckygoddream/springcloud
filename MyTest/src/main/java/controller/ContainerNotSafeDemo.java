2eeeb1ac5d533258d16eda4f6cb843370cd233ed
jwq0204@163.com <jwq0204@163.com>
2021-01-26 16:40:08 +0800

    锁学习

--- MyTest/src/main/java/controller/ContainerNotSafeDemo.java
+++ MyTest/src/main/java/controller/ContainerNotSafeDemo.java
@@ -1,6 +1,9 @@
 package controller;
 
+import com.sun.corba.se.spi.extension.CopyObjectPolicy;
+
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**集合类线程不安全的问题
  * ArrayList
@@ -20,15 +23,26 @@ public class ContainerNotSafeDemo {
         stringList.forEach(System.out::println);
 
         //多线程下同时添加元素会有ConcurrentModificationException错
-        List<String> strings = new ArrayList<>();
+        //List<String> strings = new ArrayList<>();
+        List<String> list = new CopyOnWriteArrayList<>();
 
-        for (int i = 0; i < 30 ; i++) {
-            new Thread(()->{
-                strings.add(UUID.randomUUID().toString().substring(0,8));
-                System.out.println(strings);
-            }).start();
-        }
+        //HashSet线程也不安全
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        Iterator<Integer> iterator = set.iterator();
+        Object next = iterator.next();
+        System.out.println(next);
+
+
+//        for (int i = 0; i < 30 ; i++) {
+//            new Thread(()->{
+//                list.add(UUID.randomUUID().toString().substring(0,8));
+//                System.out.println(list);
+//            }).start();
+//        }
         //解决方案1： List<String> strings = new Vector<>(); 不推荐使用，并发下性能急剧下降
-        //解决方案2： List list = Collections.synchronizedList(new ArrayList<>());
+        //解决方案2： List list = Collections.synchronizedList(new ArrayList<>()); 锁太重
+        //解决方案3： List list = new CopyOnWriteArrayList<>();
+
     }
 }
---
+++ MyTest/src/main/java/controller/SpinLockDemo.java
@@ -0,0 +1,59 @@
+package controller;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @Author jwq
+ * @Date 2021/1/26 16:05
+ * 自旋锁例子
+ *
+ * 自旋锁：spinlock，是指尝试获取锁的线程不会立即阻塞，而是采用循环的方式去尝试获取锁，这样的好处是减少线程上下文切换的消耗，缺点是循环会消耗CPU
+ */
+public class SpinLockDemo {
+    AtomicReference<Thread> atomicReference = new AtomicReference<>();
+    public void myLock(){
+        Thread thread = Thread.currentThread();
+        System.out.println(Thread.currentThread().getName()+"\t come in O(∩_∩)O哈哈~");
+         while (!atomicReference.compareAndSet(null,thread)){
+             System.out.println(Thread.currentThread().getName());
+         }
+    }
+
+    public void myUnLock(){
+        Thread thread = Thread.currentThread();
+        atomicReference.compareAndSet(thread,null);
+        System.out.println(Thread.currentThread().getName()+"\t muUnLock()");
+    }
+    public static void main(String[] args) {
+        SpinLockDemo spinLockDemo = new SpinLockDemo();
+        new Thread(()->{
+            spinLockDemo.myLock();
+            try {
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            spinLockDemo.myUnLock();
+        },"first").start();
+
+
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+
+        new Thread(()->{
+            spinLockDemo.myLock();
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            spinLockDemo.myUnLock();
+        },"second").start();
+    }
+}
